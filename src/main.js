0bbc90b61d9ceb05b04454a9a4559941d5f59b43
Robert Lee <bobbydwise1@hotmail.com>
2019-01-11 16:52:31 -0800

    Final commit before E.O.D. for UI and BL.

--- src/main.js
+++ src/main.js
@@ -7,10 +7,24 @@ import { Planet, AgeMass, mvemjsunp } from './solar.js';
 $(document).ready(function() {
   $("#solarAgeForm").submit(function(event){
     event.preventDefault()
-    $("input:checkbox[name=le]:checked").each(function(){let smoker = 1})
-    $("input:checkbox[name=le]:checked").each(function(){let fat = 1})
-    $("input:checkbox[name=le]:checked").each(function(){let diabetic = 1})
-    $("input:checkbox[name=le]:checked").each(function(){let excercise = 1})
+    let age = $("#user_age").val();
+    let smoker = $('input[type=checkbox]#smoker').prop('checked')*1;
+    let fat = $('input[type=checkbox]#fat').prop('checked')*1;
+    let diabetic = $('input[type=checkbox]#diabetic').prop('checked')*1;
+    let excercise = $('input[type=checkbox]#excercise').prop('checked')*1;
     console.log("made it: " + age + smoker + fat + diabetic + excercise)
+
+    let user_info = new AgeMass(age, 100);
+
+    $("#output-mercury").text(user_info.whatIsMyAgeOn(mvemjsunp[0].year).toFixed(2));
+    $("#output-venus").text(user_info.whatIsMyAgeOn(mvemjsunp[1].year).toFixed(2));
+    $("#output-earth").text(user_info.whatIsMyAgeOn(mvemjsunp[2].year).toFixed(2));
+    $("#output-mars").text(user_info.whatIsMyAgeOn(mvemjsunp[3].year).toFixed(2));
+    $("#output-jupiter").text(user_info.whatIsMyAgeOn(mvemjsunp[4].year).toFixed(2));
+    $("#output-saturn").text(user_info.whatIsMyAgeOn(mvemjsunp[5].year).toFixed(2));
+    $("#output-uranus").text(user_info.whatIsMyAgeOn(mvemjsunp[6].year).toFixed(2));
+    $("#output-neptune").text(user_info.whatIsMyAgeOn(mvemjsunp[7].year).toFixed(2));
+    $("#output-pluto").text(user_info.whatIsMyAgeOn(mvemjsunp[8].year).toFixed(2));
+    $("#output-lifeExpect").text(user_info.howManyEarthYearsLeft(smoker,fat,diabetic,excercise).toFixed(2));
   });
 });

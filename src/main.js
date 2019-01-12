b981348c90286617db449bdfa67d23059c4ed099
Robert Lee <bobbydwise1@hotmail.com>
2019-01-11 16:07:48 -0800

    Start UI.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import './styles.css';
 import { Planet, AgeMass, mvemjsunp } from './solar.js';
+
+$(document).ready(function() {
+  $("#solarAgeForm").submit(function(event){
+    event.preventDefault()
+    $("input:checkbox[name=le]:checked").each(function(){let smoker = 1})
+    $("input:checkbox[name=le]:checked").each(function(){let fat = 1})
+    $("input:checkbox[name=le]:checked").each(function(){let diabetic = 1})
+    $("input:checkbox[name=le]:checked").each(function(){let excercise = 1})
+    console.log("made it: " + age + smoker + fat + diabetic + excercise)
+  });
+});
